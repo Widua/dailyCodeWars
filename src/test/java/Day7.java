810aa67d20a286f99ddb7c897ad43167a846e386
Widua <mikolaj.widla@gmail.com>
2022-06-19 16:23:35 +0200

    Day 7 completed! https://www.codewars.com/kata/5842df8ccbd22792a4000245

---
+++ src/test/java/Day7.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day7 {
+
+    public String expandedForm(int num){
+        String fullNumber = String.valueOf(num) ;
+        String[] elements = fullNumber.split("(?=[1-9])");
+        StringBuilder fin = new StringBuilder();
+
+        int fullLength = fullNumber.length() ;
+        int actualLength = 0 ;
+
+        for (int i = 0 ; i < fullLength ; i++ ){
+            if (actualLength == fullLength){
+                break;
+            }
+            actualLength += elements[i].length() ;
+
+            fin.append(" + ").append(Integer.parseInt(elements[i]) * (int) Math.pow (10,fullLength - actualLength));
+        }
+
+
+        return fin.toString().substring(3);
+    }
+
+
+
+
+    @Test
+    public void basicNumbersTest(){
+        assertEquals("10 + 2" , expandedForm(12)) ;
+        assertEquals("40 + 2" , expandedForm(42));
+    }
+
+    @Test
+    public void moreComplicatedNumbers(){
+        assertEquals("400000 + 300 + 1" , expandedForm(400301));
+    }
+
+}
